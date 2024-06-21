46e78420ceb1577b148541e063ce42c771f6aeaf
CdawgfromtheAncientLands <cmrbusiness123456@gmail.com>
2024-06-21 19:34:45 -0400

    Configure linear scale for bar widths.

--- cc_11.js
+++ cc_11.js
@@ -1,17 +1,21 @@
-        // Data
-        const dataset = [100, 420, 230, 850, 560, 925];
+// Data
+const dataset = [100, 420, 230, 850, 560, 925];
 
-        // Dimensions
-        const margin = { top: 20, right: 20, bottom: 20, left: 20 };
-        const width = 500 - margin.left - margin.right;
-        const barHeight = 20;
-        const barMargin = 1;
-        const height = (barHeight + barMargin) * dataset.length;
+// Dimensions
+const margin = { top: 20, right: 20, bottom: 20, left: 20 };
+const width = 500 - margin.left - margin.right;
+const barHeight = 20;
+const barMargin = 1;
+const height = (barHeight + barMargin) * dataset.length;
 
-        // Create SVG container
-        const svg = d3.select("body")
-            .append("svg")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-            .append("g")
-            .attr("transform", `translate(${margin.left},${margin.top})`);
+// Create SVG container
+const svg = d3.select("body")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
+    
+const xScale = d3.scaleLinear()
+    .domain([0, d3.max(dataset)])
+    .range([0, width]);
